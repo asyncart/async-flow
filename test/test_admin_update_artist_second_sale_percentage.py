62acad0fe490a185aa6871bfc50e74a63048d885
Sam O <sam.orend22@gmail.com>
2021-12-25 04:10:54 -0500

    fixed naming

--- test/test_admin_update_artist_second_sale_percentage.py
+++ test/test_admin_update_artist_second_sale_percentage.py
@@ -10,11 +10,11 @@ def test_update_artist_second_sale_percentage():
   # Deploy contracts
   main()
 
-  newPlatformFeePercentages = [["UFix64", "2.0"]]
-  assert send_transaction("updateArtistSecondSalePercentage", args=newPlatformFeePercentages, signer="AsyncArtAccount")
+  newArtistSecondSalePercentage = [["UFix64", "2.0"]]
+  assert send_transaction("updateArtistSecondSalePercentage", args=newArtistSecondSalePercentage, signer="AsyncArtAccount")
   assert 2.0 == float(send_script_and_return_result("getArtistSecondSalePercentage"))
   assert check_for_event("A.01cf0e2f2f715450.AsyncArtwork.ArtistSecondSalePercentUpdated")
-  print("Successfully Updated Default Platform Sales Percentages")
+  print("Successfully Updated Artist Second Sale Percentage")
 
 if __name__ == '__main__':
     test_update_artist_second_sale_percentage()
